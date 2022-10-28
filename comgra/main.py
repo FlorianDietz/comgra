4c65ebeb287d5223713254a07caf480ae9302d60
fd <floriandietz44@gmail.com>
2022-10-28 21:11:15 +0200

    .

---
+++ comgra/main.py
@@ -0,0 +1,5 @@
+
+
+def test():
+    print(123)
+    assert False, "foobar"
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name='comgra',
+      version='0.1.0',
+      description='',
+      url='https://github.com/FlorianDietz/comgra',
+      author='Florian Dietz',
+      author_email='floriandietz44@gmail.com',
+      packages=['comgra']
+)
