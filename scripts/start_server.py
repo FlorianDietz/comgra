6c13a97fdb9a06027797130c647b940525d7413a
fdietz <floriandietz44@gmail.com>
2022-11-02 11:18:24 +0100

    .

--- scripts/start_server.py
+++ scripts/start_server.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run an experiment.")
     parser.add_argument('--name', dest='name', default=None)
     args = parser.parse_args()
-    path = Path("/home/remote/PycharmProjects/AI22/comgra_data") / args.name
+    path = Path(__file__).parent.parent.parent / 'comgra_data' / args.name
     assert path.exists(), path
     vis = visualizer.Visualization(path=path)
     vis.run_server()
