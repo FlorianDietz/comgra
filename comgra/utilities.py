21883bffadfe321092343fae424da89fc95719fc
fd <floriandietz44@gmail.com>
2022-11-26 15:21:02 +0100

    .

--- comgra/utilities.py
+++ comgra/utilities.py
@@ -22,26 +22,30 @@ def runtime_analysis_decorator(function):
             print("  " * _CURRENT_INDENT + f"START {function.__name__}")
         _CURRENT_INDENT += 1
         _CALL_STACK.append(function.__name__)
-        res = function(*args, **kwargs)
-        _CURRENT_INDENT -= 1
-        _CALL_STACK.pop()
-        end = datetime.now()
-        duration = end - start
-        FUNCTION_NAME_TO_TOTAL_TIME[function_name] += duration
-        FUNCTION_NAME_TO_TOTAL_NUM_CALLS[function_name] += 1
-        # Subtract the time spent in a function from its parent function.
-        # This is to avoid double-counting, since the parent has its own time tracking.
-        # This is particularly bad if a function is recursive,
-        # because it will look many times more expensive than it is.
-        if _CALL_STACK:
-            parent = _CALL_STACK[-1]
-            FUNCTION_NAME_TO_TOTAL_TIME[parent] -= duration
-        total_duration_in_this_function = FUNCTION_NAME_TO_TOTAL_TIME[function_name]
-        if PRINT_EACH_TIME:
-            print(
-                "  " * _CURRENT_INDENT + f"END {function_name} after {duration}, "
-                                         f"total duration {total_duration_in_this_function}"
-            )
+        try:
+            res = function(*args, **kwargs)
+        except Exception as e:
+            raise
+        finally:
+            _CURRENT_INDENT -= 1
+            _CALL_STACK.pop()
+            end = datetime.now()
+            duration = end - start
+            FUNCTION_NAME_TO_TOTAL_TIME[function_name] += duration
+            FUNCTION_NAME_TO_TOTAL_NUM_CALLS[function_name] += 1
+            # Subtract the time spent in a function from its parent function.
+            # This is to avoid double-counting, since the parent has its own time tracking.
+            # This is particularly bad if a function is recursive,
+            # because it will look many times more expensive than it is.
+            if _CALL_STACK:
+                parent = _CALL_STACK[-1]
+                FUNCTION_NAME_TO_TOTAL_TIME[parent] -= duration
+            total_duration_in_this_function = FUNCTION_NAME_TO_TOTAL_TIME[function_name]
+            if PRINT_EACH_TIME:
+                print(
+                    "  " * _CURRENT_INDENT + f"END {function_name} after {duration}, "
+                                             f"total duration {total_duration_in_this_function}"
+                )
         return res
     return wrapper
 
