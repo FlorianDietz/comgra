a7d45795a343a1d93084775393b479f32edc4e69
fdietz <floriandietz44@gmail.com>
2022-12-08 12:06:00 +0100

    .

--- scripts/start_server.py
+++ scripts/start_server.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
     parser.add_argument('--name', dest='name', default=None)
     parser.add_argument('--port', dest='port', default=None)
     args = parser.parse_args()
-    path = Path(__file__).parent.parent.parent / 'comgra_data' / args.name
+    # path = Path(__file__).parent.parent.parent / 'comgra_data' / args.name
+    path = Path("/data/users/fdietz/comgra_data") / args.name
     assert path.exists(), path
     vis = visualizer.Visualization(path=path)
     vis.run_server(args.port)
