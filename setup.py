1c960287a2e7e4cd6e1d01fd9d445b415c5ff865
fd <floriandietz44@gmail.com>
2023-03-03 12:40:50 +0100

    .

--- setup.py
+++ setup.py
@@ -8,11 +8,4 @@ setup(
       author='Florian Dietz',
       author_email='floriandietz44@gmail.com',
       packages=['comgra'],
-      install_requires=[
-            'dash==2.6.2',
-            'dash-svg==0.0.12',
-            'msgpack==1.0.4',
-            'numpy==1.24.2',
-            'pandas==1.5.3',
-      ],
 )
