426e8453f7f2f9ad33f01716c0dde7165b4b0017
fd <floriandietz44@gmail.com>
2024-07-01 09:32:11 +0200

    fix server to work when used through a package manager (pickle does not work on nested functions)

--- src/scripts/server.py
+++ src/scripts/server.py
@@ -7,6 +7,8 @@ from typing import Optional
 
 from comgra import visualizer
 
+restart_signal_queue = multiprocessing.Queue()
+
 def main():
     parser = argparse.ArgumentParser(description="Run an experiment.")
     parser.add_argument('--path', dest='path', default=None)
@@ -24,34 +26,24 @@ def main():
     if args.use_path_for_test_run:
         args.path = (Path(__file__).parent.parent.parent / 'testing_data' / 'testcase_for_demonstration').absolute()
         args.external_visualization_file = (Path(__file__).parent / 'example_custom_visualization.py').absolute()
-    path = Path(args.path).absolute()
-    assert path.exists(), path
-    restart_signal_queue = multiprocessing.Queue()
-    the_server_process: Optional[multiprocessing.Process] = None
-
-    def run_visualization():
-        vis = visualizer.Visualization(
-            path=path,
-            debug_mode=args.debug_mode,
-            external_visualization_file=args.external_visualization_file,
-            restart_signal_queue=restart_signal_queue,
-        )
-        vis.run_server(args.port, use_reloader=args.debug_mode)
+    full_path = Path(args.path).absolute()
+    assert full_path.exists(), full_path
+    args.full_path = full_path
 
     # Either run in a loop that allows the user to manually restart the server,
     # or use debug_mode and Dash's built-in use_reloader.
+    the_server_process: Optional[multiprocessing.Process] = None
+
+    def start_or_restart_server():
+        nonlocal the_server_process
+        if the_server_process is not None:
+            the_server_process.terminate()
+            the_server_process.join()
+        the_server_process = multiprocessing.Process(target=run_visualization, args=(args,))
+        the_server_process.start()
     if args.debug_mode:
-        run_visualization()
+        run_visualization(args)
     else:
-
-        def start_or_restart_server():
-            nonlocal the_server_process
-            if the_server_process is not None:
-                the_server_process.terminate()
-                the_server_process.join()
-            the_server_process = multiprocessing.Process(target=run_visualization)
-            the_server_process.start()
-
         start_or_restart_server()
         # Infinite loop to keep the main thread alive,
         # while listening for a signal to restart the server
@@ -59,6 +51,16 @@ def main():
             msg = restart_signal_queue.get()
             if msg == 'restart':
                 start_or_restart_server()
+
+def run_visualization(args):
+    vis = visualizer.Visualization(
+        path=args.full_path,
+        debug_mode=args.debug_mode,
+        external_visualization_file=args.external_visualization_file,
+        restart_signal_queue=restart_signal_queue,
+    )
+    vis.run_server(args.port, use_reloader=args.debug_mode)
+
 
 if __name__ == '__main__':
     main()
