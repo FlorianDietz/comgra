2bc8ed43ff9147c171de839bb03d67aed0f2c855
fd <floriandietz44@gmail.com>
2024-07-11 15:44:35 +0200

    Revert "DEBUG - measure runtime in preparation for modifications"
    This reverts commit 4daa157e31a5af4880bb06d16f30e1ce77b83b44.

--- src/comgra/utilities_initialization_config.py
+++ src/comgra/utilities_initialization_config.py
@@ -2,5 +2,5 @@
 # These are defined in a separate file and then imported in utilities.py,
 # so that it is possible to change them before importing utilities.py
 # This is necessary for decorators to work correctly.
-DEBUG_MODE = True
+DEBUG_MODE = False
 PRINT_EACH_TIME = False
