3e9ccc358656d04a910e6c1107b34f44da328f5e
fdietz <floriandietz44@gmail.com>
2024-08-30 12:33:34 +0200

    .

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from comgra import objects, recorder, utilities_initialization_config, utilities, visualizer
+    my_recorder: recorder.ComgraRecorder
